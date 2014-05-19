6d2b951a576db7a07edd4f11bdde9122e4ccd4b9
Evan Kim <evan.kim.390@gmail.com>
2014-05-19 10:59:19 -0700

    Initial Commit

---
+++ client/app.js
@@ -0,0 +1,11 @@
+(function (angular) {
+  "use strict";
+  angular.module('SoundCloudRadio', [
+    'fx.animations',
+    'ngAnimate',
+    'ngRoute'
+, 'SoundCloudRadio.main']);
+}(angular));
+
+
+
