3323c12dc345dfae86f2a099a058539b4759ab80
Evan Kim <evan.kim.390@gmail.com>
2014-05-20 10:18:52 -0700

    Automated scraper working

--- server/main/request-handler.js
+++ server/main/request-handler.js
@@ -250,3 +250,214 @@ exports.fetchSuggestions = function(req, res){
 
   res.send(200);
 }
+
+exports.fetchSuggestionsInternal = function(req, res){
+  var possiblyNewTracks = {}; //Save this in the closure scope for use later
+  var suggestions = [];
+
+  //Clean up the url a bit, remove https
+  req.url = req.url.substring(1);
+  //Query database to see if this track is already there
+
+  return new Promise(function(resolveAll, rejectAll){
+    findTrack({url: parseLookupUrl(req.url)})
+    .then(function(track){
+
+      if(track === null){
+
+        return new Promise(function(resolve, reject){
+          request({
+            url: parseRequest(req.url),
+            json: true
+          }).then(function(data){
+            var track = data[0].body;
+            var newTrack = new Track;
+            newTrack.url = track.permalink_url;
+            newTrack.title = track.title;
+            newTrack.scId = track.id;
+            newTrack.plays = track.playback_count;
+            var save = Promise.promisify(newTrack.save, newTrack);
+            return save()
+          }).then(function(track){
+            resolve(track[0]);
+          });
+        });
+
+      } else {
+        return new Promise(function(resolve, reject){
+          resolve(track);
+        });
+      }
+    }).then(function(track){ //Now that we have the track, either push the favoriters forward or look them up
+      console.log('Looking up favoriters');
+      if(track.favoriters.length === 0){ //This promise should return an array of user IDs
+        return new Promise(function(resolve, reject){
+          request({
+            url: parseRequest('/tracks/' + track.scId + '/favoriters'),
+            json: true
+          }).then(function(data){
+            var favoriters = data[0].body;
+            for(var i = 0; i < favoriters.length; i++){
+              track.favoriters.push(favoriters[i].id);
+            };
+            var save = Promise.promisify(track.save, track);
+            return save();
+          }).then(function(track){
+            resolve(track[0].favoriters);
+          });
+        });
+
+      } else {
+        return new Promise(function(resolve, reject){
+          resolve(track.favoriters);
+        });
+      }
+    }).then(function(favoriters){ //Now we have an array of user IDs given to us. Ensure that each user is in the database and has an array of favorites.
+      //Fulfill this promise with an object containing trackId and number of users who like that track.
+
+      //Set up all of the promises
+      var promises = [];
+      var userIds = [];
+      var userHolder = [];
+      var insertionIds = [];
+
+      for(var i = 0; i < favoriters.length; i++){
+        var userId = favoriters[i];
+        userIds.push(userId);
+        promises.push(findUser({scId: userId}));
+      }
+
+      Promise.all(promises).then(function(users){
+        return new Promise(function(resolve, reject){
+          var promises = [];
+          for(var i = 0; i < users.length; i++){
+            if(users[i] === null){
+              var newUser = new User;
+              newUser.scId = userIds[i];
+              var save = Promise.promisify(newUser.save, newUser);
+              promises.push(save());
+            } else {
+              promises.push(users[i]);
+            }
+          };
+          Promise.all(promises).then(function(users){
+            resolve(users);
+          });
+        });
+      }).then(function(users){ //Go through users, ensuring that they have favorites. If not, then HTTP get them!
+        console.log('Looking up favorites');
+        userHolder = users;
+        return new Promise(function(resolve, reject){
+          var promises = [];
+          for(var i = 0; i < users.length; i++){
+            if(Array.isArray(users[i])){
+              users[i] = users[i][0];
+            }
+            if(users[i].favorites.length === 0){
+              var queryString = '/users/' + users[i].scId + '/favorites';
+              promises.push(request({
+                url: parseRequest(queryString),
+                json: true
+              }));
+            } else {
+              promises.push(users[i]);
+            }
+          };
+          Promise.all(promises).then(function(users){
+            resolve(users);
+          });
+        });
+      }).then(function(data){ //We have an array with some users and some objects with a body property
+        var promises = [];
+        return new Promise(function(resolve, reject){
+          for(var i = 0; i < data.length; i++){
+            //If favorites had to be added, data[i][0].body contains the list of favorites
+            //If no favorites had to be added, data[i] is the user object itself
+            var keys = Object.keys(data[i]);
+
+            if(keys.indexOf('save') > -1){
+              promises.push(userHolder[i]);
+            } else {
+              var temp = [];
+              var favorites = data[i][0].body;
+              for(var j = 0; j < favorites.length; j++){
+                var track = favorites[j];
+                var trackId = favorites[j].id;
+                possiblyNewTracks[trackId] = {
+                  title: track.title,
+                  url: track.permalink_url,
+                  scId: trackId,
+                  plays: track.playback_count
+                };
+                temp.push(trackId);
+              }
+              userHolder[i].favorites = temp;
+              var save = Promise.promisify(userHolder[i].save, userHolder[i]);
+              promises.push(save());
+            }
+          }
+          Promise.all(promises).then(function(users){
+            resolve(users);
+          });
+        });
+      }).then(function(users){ //FINALLY we have all the users who have favorited the track and their favorites. PARSE!!!!
+        //If the user is already in the database, an array of arrays of users is returned
+        //Else an object is returned
+        var tracks = {};
+        
+        for(var i = 0; i < users.length; i++){
+          if(typeof users[i][0] === 'undefined'){
+            user = users[i];
+          } else {
+            user = users[i][0];
+          }
+          for(var j = 0; j < user.favorites.length; j++){
+            var trackId = user.favorites[j];
+            tracks[trackId] ? tracks[trackId]++ : tracks[trackId] = 1;
+          }
+        }
+        for(var key in tracks){
+          if(tracks[key] > 5){
+            suggestions.push(key);
+            suggestions.sort(function(a,b){
+              return tracks[a] - tracks[b];
+            });
+          }
+        }
+        return new Promise(function(resolve, reject){
+          resolve();
+        });
+      }).then(function(){ //We've saved the users and their favorites to the database, as well as their tracks. Now we need to save any tracks 
+      //which aren't already in the db
+        console.log('Saving new tracks to database');
+        return new Promise(function(resolve, reject){
+          var promises = [];
+          var trackIds = Object.keys(possiblyNewTracks);
+          for(var i = 0; i < trackIds.length; i++){
+            insertionIds.push(trackIds[i]);
+            promises.push(findTrack({scId: trackIds[i]}));
+          }
+          Promise.all(promises).then(function(data){
+            resolve(data);
+          });
+        });
+      }).then(function(data){
+        return new Promise(function(resolve, reject){
+          var promises = [];
+          for(var i = 0; i < data.length; i++){
+            if(data[i] === null){ //Then we need to save a new track to the database
+              var newTrack = new Track(possiblyNewTracks[insertionIds[i]]);
+              var save = Promise.promisify(newTrack.save, newTrack);
+              promises.push(save());
+            }
+          }
+          Promise.all(promises).then(function(data){
+            resolve('promise called');
+          });
+        });
+      }).then(function(data){ //Finally we fetch the metadata for all the trackIDs we have
+        resolveAll('track data updated');
+      });
+    });
+  });
+}
--- server/main/scraper.js
+++ server/main/scraper.js
@@ -5,13 +5,16 @@ var Track = require('./models/track');
 
 var findOneTrack = Promise.promisify(Track.findOne, Track);
 
-var fetchTrack = function(minPlays){
+var minPlays = 10000000;
+
+var fetchTrack = function(){
   return new Promise(function(resolve, reject){
     findOneTrack({
       favoriters: { $size: 0 },
       plays: { $gt: minPlays }
     }).then(function(track){
       if(track === null){
+        minPlays -= 50000;
         scrape(minPlays - 50000);
       } else {
         resolve(track);
@@ -20,17 +23,27 @@ var fetchTrack = function(minPlays){
   });
 };
 
-var scrape = function(minPlays){
+var scrape = function(){
   fetchTrack(minPlays)
   .then(function(track){
+    console.log('##########################################################################');
+    console.log('Fetching data for: ', track.title);
+    console.log('Number of plays: ', track.plays);
     var array = track.url.split('');
     array.splice(5, 0 , 's');
     var queryUrl = array.join('');
-    handler.fetchSuggestions({url: '/' + queryUrl}, {send: function(){}});
+    return handler.fetchSuggestionsInternal({url: '/' + queryUrl}, {send: function(){}});
   })
-  .then(function(){
-    console.log('complete');
+  .then(function(resolutionMessage){
+    console.log('Fetch complete');
+    minPlays += 50000;
+    scrape(minPlays);
   });
 };
 
+var initialize = function(){
+  minPlays = 10000000;
+};
+
+initialize();
 scrape(10000000);
